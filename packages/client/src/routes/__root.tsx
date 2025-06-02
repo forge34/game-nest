d5bf458aede615167eb4812d007a397038009772
Forge <28455546+forge34@users.noreply.github.com>
2025-06-02 22:11:21 +0300

    feat(client): initial style setup + initial sidebar design

--- packages/client/src/routes/__root.tsx
+++ packages/client/src/routes/__root.tsx
@@ -1,4 +1,9 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import styles from "./root.module.css";
+import Logo from "../assets/home.svg?react";
+import Library from "../assets/library.svg?react";
+import Search from "../assets/search.svg?react";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -7,7 +12,22 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
+      <div className={styles.headerContainer}>
+        <Link to="/" className="sidebar-link">
+          <Logo className="icon" width={18} height={18} />
+          Home
+        </Link>
+        <Link className={"sidebar-link"} to="/">
+          <Search className="icon " width={18} height={18} />
+          Browse Games
+        </Link>
+        <Link className={"sidebar-link"} to="/">
+          <Library className="icon invert" width={18} height={18} />
+          Library
+        </Link>
+      </div>
       <Outlet />
+      <TanStackRouterDevtools />
     </>
   );
 }
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import vitePluginSvgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
+    vitePluginSvgr()
   ],
 });
