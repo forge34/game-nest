2b47a1103f198db632dddab35c529c2bca661ddd
Forge <28455546+forge34@users.noreply.github.com>
2025-06-27 22:03:28 +0300

    refactor: prepare to refactor

--- packages/client/src/routes/index.tsx
+++ packages/client/src/routes/index.tsx
@@ -1,10 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import styles from "./index.module.css";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div className={styles.container}> </div>;
+  return <div > </div>;
 }
