27055472aa49b4a23283e73f16f8a6a6e1b73aed
Forge <28455546+forge34@users.noreply.github.com>
2025-06-30 23:55:50 +0300

    feat: initial category filter setup for /browse

---
+++ packages/client/src/components/horizontal-filter.tsx
@@ -0,0 +1,80 @@
+import type { GenresWithGames } from "@game-forge/shared";
+import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Checkbox } from "./ui/checkbox";
+import { Badge } from "./ui/badge";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface GenreFilterProps {
+  filters: GenresWithGames[];
+  selectedGenres: string[];
+  onChangeChecked: (selectedGenres: string[]) => void;
+}
+
+function GenreFilter({
+  filters,
+  selectedGenres,
+  onChangeChecked,
+}: GenreFilterProps) {
+  function togglefilter(genre: string) {
+    if (selectedGenres.includes(genre)) {
+      onChangeChecked(selectedGenres.filter((g) => g !== genre));
+    } else {
+      onChangeChecked([...selectedGenres, genre]);
+    }
+  }
+
+  return (
+    <div className="flex flex-row w-full justify-between">
+      <Popover>
+        <PopoverTrigger>
+          <Button variant="outline">Categories</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          {filters.map((genre) => (
+            <div className="flex flex-row my-3">
+              <p>{genre.name}</p>
+
+              <Badge variant="outline" className="ml-auto mr-2">
+                {genre.games.length}
+              </Badge>
+              <Checkbox
+                value={genre.name}
+                onCheckedChange={() => togglefilter(genre.name)}
+              />
+            </div>
+          ))}
+        </PopoverContent>
+      </Popover>
+      <div className="ml-auto">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Sort by</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>Sort filters</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioGroup>
+              <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="bottom">
+                Bottom
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+}
+
+export default GenreFilter;
--- packages/client/src/routes/browse.tsx
+++ packages/client/src/routes/browse.tsx
@@ -1,9 +1,34 @@
-import { createFileRoute } from '@tanstack/react-router'
+import GenreFilter from "@/components/horizontal-filter";
+import type { GenresWithGames } from "@game-forge/shared";
+import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
-export const Route = createFileRoute('/browse')({
+export const Route = createFileRoute("/browse")({
   component: RouteComponent,
-})
+  loader: async (): Promise<GenresWithGames[]> => {
+    const res = await fetch(`${import.meta.env.VITE_API}/genres`, {
+      mode: "cors",
+    });
+
+    return await res.json();
+  },
+});
 
 function RouteComponent() {
-  return <div>Hello "/browse"!</div>
+  const data = Route.useLoaderData();
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+
+  function onFilter(genres: string[]) {
+    setSelectedGenres(genres);
+  }
+
+  return (
+    <div className="flex flex-row mx-6 my-4 ">
+      <GenreFilter
+        filters={data}
+        selectedGenres={selectedGenres}
+        onChangeChecked={onFilter}
+      />
+    </div>
+  );
 }
