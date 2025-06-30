7ea44bfed03cdb25554dab59329471899304f968
Forge <28455546+forge34@users.noreply.github.com>
2025-06-30 17:50:50 +0300

    refactor(api): refactor /games to return cover and other join objects

--- packages/server/src/controllers/games.ts
+++ packages/server/src/controllers/games.ts
@@ -3,21 +3,29 @@ import { Response, Request } from "express";
 
 const GamesRoute = {
   findMany: async (req: Request, res: Response) => {
-    const games = await prisma.game.findMany();
+    const games = await prisma.game.findMany({
+      include: {
+        coverImage: true,
+        genres:true
+      },
+    });
 
     res.status(200).json(games);
   },
 
-  findOneById: async (req:Request,res:Response) => {
-    const id = parseInt(req.params.id)
+  findOneById: async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
     const game = await prisma.game.findFirst({
-      where:{
-        id:id
-      }
-    })
+      where: {
+        id: id,
+      },
+      include: {
+        coverImage: true,
+      },
+    });
 
-    res.status(200).json(game)
-  }
+    res.status(200).json(game);
+  },
 };
 
-export default GamesRoute
+export default GamesRoute;
