88d957bd29b3ba0415acb0fc95d010a7232b13af
Forge <28455546+forge34@users.noreply.github.com>
2025-06-30 14:20:53 +0300

    chore(client): setup shadcn

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import vitePluginSvgr from "vite-plugin-svgr";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
-    vitePluginSvgr()
+    vitePluginSvgr(),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
