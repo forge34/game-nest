f41b64db5a80f2473c67d8294b281318e7e4903f
Forge <28455546+forge34@users.noreply.github.com>
2025-06-30 15:25:22 +0300

    feat(client): add top nav bar

--- packages/client/src/routes/__root.tsx
+++ packages/client/src/routes/__root.tsx
@@ -1,5 +1,9 @@
-import { createRootRoute } from "@tanstack/react-router";
+import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-proivder";
+import { Button } from "@/components/ui/button";
+import { createRootRoute, Link } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Globe, Home, Info, Library } from "lucide-react";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -7,8 +11,37 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <>
+    <ThemeProvider>
+      <nav className="flex flex-row py-2 border  px-4 items-center">
+        <span className="flex flex-row justify-around w-full">
+          <span className="flex flex-row">
+            <Home size={"1rem"} className="mx-2" />
+            <Link to="/">Home</Link>
+          </span>
+          <span className="flex flex-row">
+            <Globe size={"1rem"} className="mx-2" />
+            <Link to="/">Browse</Link>
+          </span>
+          <span className="flex flex-row">
+            <Library size={"1rem"} className="mx-2" />
+            <Link to="/">My library</Link>
+          </span>
+          <span className="flex flex-row">
+            <Info size={"1rem"} className="mx-2" />
+            <Link to="/">About</Link>
+          </span>
+        </span>
+        <span className="flex flex-row gap-4 ml-auto">
+          <Button>
+            <Link to="/">Login</Link>
+          </Button>
+          <Button>
+            <Link to="/">Create Account</Link>
+          </Button>
+          <ModeToggle />
+        </span>
+      </nav>
       <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   );
 }
