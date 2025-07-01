fa4dcfaadf7bb7fb7db0d38d0b8ea3247452c6c4
Forge <28455546+forge34@users.noreply.github.com>
2025-07-02 00:23:24 +0300

    refactor(client): extract query client to a separate file

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,13 +1,30 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import "./index.css"
+import {
+  Outlet,
+  RouterProvider,
+  createRootRouteWithContext,
+  createRouter,
+} from "@tanstack/react-router";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import { queryClient } from "./api";
 
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  component: () => <Outlet />,
+});
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -23,7 +40,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
