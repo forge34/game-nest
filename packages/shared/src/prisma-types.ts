16b1030d1d7c8c0e9b2dd378f31de3e7c2036ea4
Forge <28455546+forge34@users.noreply.github.com>
2025-07-01 23:09:44 +0300

    feat(shared): add User type

--- packages/shared/src/prisma-types.ts
+++ packages/shared/src/prisma-types.ts
@@ -7,12 +7,19 @@ export type GamesAllIncluded = Prisma.GameGetPayload<{
     platforms: true;
     parent_game: true;
     companies: true;
-    reviews:true
+    reviews: true;
   };
 }>;
 
 export type GenresWithGames = Prisma.GenreGetPayload<{
-  include:{
-    games:true
-  }
-}>
+  include: {
+    games: true;
+  };
+}>;
+
+export type User = Prisma.UserGetPayload<{
+  include: {
+    reviews: true;
+    favourites: true;
+  };
+}>;
