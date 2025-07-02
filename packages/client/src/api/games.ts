14509c8ab385ce62943c221e9e02aad0cb01dcc2
Forge <28455546+forge34@users.noreply.github.com>
2025-07-03 00:30:00 +0300

    feat(client): fetch api data using react query

---
+++ packages/client/src/api/games.ts
@@ -0,0 +1,23 @@
+import { safeFetch } from "@/utils";
+import type { GamesAllIncluded, GenresWithGames } from "@game-forge/shared";
+import { queryOptions } from "@tanstack/react-query";
+
+const getAllGames = () =>
+  queryOptions({
+    queryKey: ["game"],
+    queryFn: () => safeFetch<GamesAllIncluded[]>("games", {}),
+  });
+
+const getGameById = (id: string) =>
+  queryOptions({
+    queryKey: ["game", id],
+    queryFn: () => safeFetch<GamesAllIncluded>(`game/${id}`, {}),
+  });
+
+const getAllGenres = () =>
+  queryOptions({
+    queryKey: ["genre"],
+    queryFn: () => safeFetch<GenresWithGames[]>("genres", {}),
+  });
+
+export { getAllGames, getGameById, getAllGenres };
