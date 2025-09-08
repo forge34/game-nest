d973de9aecf8a7e34786080a37a5d28e8cf5d83b
Forge <28455546+forge34@users.noreply.github.com>
2025-09-09 00:34:31 +0300

    refactor: refactor server error handler
    better improvements later

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -35,31 +35,24 @@ app.use(passport.initialize());
 app.use(router);
 
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  console.log(err);
+  console.error(err);
+
   if (err.name === "AuthenticationError") {
-    res.status(401).json({ message: "Unauthorized" });
-    return;
-  }
-  if (err instanceof Prisma.PrismaClientKnownRequestError) {
-    if (err.code === "P2002") {
-      console.log("handling");
-      const target = err.meta?.target;
-
-      const fields =
-        Array.isArray(target) && target.every((t) => typeof t === "string")
-          ? target.join(", ")
-          : "field";
-
-      res.status(409).json({
-        message: `A record with this ${fields} already exists.`,
-      });
-      return;
-    }
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
-  res
-    .status(err.code || 500)
-    .json({ message: err.message || "Internal Server Error" });
+  if (err.code === "P2002") {
+    return res.status(409).json({ message: "Duplicate entry" });
+  }
+
+  if (err.code === "P2025") {
+    return res.status(404).json({ message: "Record not found" });
+  }
+
+  const isDev = process.env.NODE_ENV !== "production";
+  res.status(500).json({
+    message: isDev ? err.message : "Internal Server Error",
+  });
 };
 
 app.use(errorHandler);
